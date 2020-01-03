0a8d633f56db52c020695d877d5d94d774e37bae
DavidGmezHdez <davidgmezhdez@gmail.com>
2020-01-03 17:29:50 +0100

    Actualizando el disparador | Posiblemente este mal

--- db/disparador5.sql
+++ db/disparador5.sql
@@ -3,9 +3,11 @@ CREATE OR REPLACE TRIGGER horaDiferencia
     BEFORE
     INSERT ON asignar
     FOR EACH ROW
+DECLARE
+    fechaL INTEGER;
+    fechaS INTEGER;
 BEGIN
-    SELECT codtrabajador FROM asignar WHERE codtrabajador = :new.codtrabajador;
-
-
-
+    SELECT to_number(to_char(fechaInicio)),to_number(to_char(fechaFin)) INTO fechaL,fechaS FROM asignar WHERE codtrabajador = :new.codtrabajador;
+    if(fechaL - fechaS < 10000) THEN
+        raise_application_error(-20600, :new.codtrabajador || 'No se le puede asignar el trabajo con menos de 1 hora de diferencia');
 END
