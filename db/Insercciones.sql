b61ce2165fbf3452802a1bb8134905ad12ed863d
migue <posadasarraez@correo.ugr.es>
2020-01-17 17:45:53 +0100

    Subiendo script fichero para la inserccion de tuplas en la base de datos

---
+++ db/Insercciones.sql
@@ -0,0 +1,20 @@
+INSERT INTO arbitros VALUES('1','manolo@correo.tk','666666666','Manolo','Manolo');
+INSERT INTO arbitros VALUES('2','juan@correo.tk','777777777','Juan','Manuel');
+INSERT INTO arbitros VALUES('3','luisa@correo.tk','555555555','Luisa','Maria');
+
+INSERT INTO ofertas VALUES('1','500');
+INSERT INTO ofertas VALUES('2','1000');
+
+INSERT INTO gestiona VALUES('1','1');
+INSERT INTO gestiona VALUES('1','2');
+
+INSERT INTO edicion VALUES('5', '1999');
+
+INSERT INTO trabajadores VALUES('5', 'manolo@fda.tl', '234234', 'manolo', 'paco');
+INSERT INTO trabajadores VALUES('6', 'asdfmanolo@fda.tl', '32234234', 'wafdmanolo', 'fasdfpaco');
+
+INSERT INTO pistas VALUES('12', 'paquillo', '2', '1234');
+
+INSERT INTO asignar VALUES('5', '5', '1999', '12', TO_DATE('12/03/1999', 'DD/MM/YYYY'), TO_DATE('12/03/1999', 'DD/MM/YYYY'));
+
+INSERT INTO asignar VALUES('5', '5', '1999', '12', TO_DATE('12/03/1999', 'DD/MM/YYYY'), TO_DATE('12/03/1999', 'DD/MM/YYYY'));
