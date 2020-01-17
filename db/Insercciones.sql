211b5d38c9fd8727e59800461d1ca4ac30e5a6c8
migue <posadasarraez@correo.ugr.es>
2020-01-17 18:42:52 +0100

    Arreglando disparador 3 y cambios menores

--- db/Insercciones.sql
+++ db/Insercciones.sql
@@ -2,13 +2,22 @@ INSERT INTO arbitros VALUES('1','manolo@correo.tk','666666666','Manolo','Manolo'
 INSERT INTO arbitros VALUES('2','juan@correo.tk','777777777','Juan','Manuel');
 INSERT INTO arbitros VALUES('3','luisa@correo.tk','555555555','Luisa','Maria');
 
-INSERT INTO ofertas VALUES('1','500');
-INSERT INTO ofertas VALUES('2','1000');
-
-INSERT INTO gestiona VALUES('1','1');
-INSERT INTO gestiona VALUES('1','2');
-
 INSERT INTO edicion VALUES('5', '1999');
+INSERT INTO edicion VALUES('6', '2000');
+
+INSERT INTO ofertas VALUES('1','1','500','5','1999');
+INSERT INTO ofertas VALUES('2','1','1000','5','1999');
+
+INSERT INTO ofertasaceptadas VALUES('1');
+
+INSERT INTO ofertas VALUES('3','1','1500','5','1999');
+INSERT INTO ofertas VALUES('4','1','1500','6','2000');
+INSERT INTO ofertas VALUES('5','2','1500','5','1999');
+
+INSERT INTO ofertasrechazadas VALUES('5');
+
+INSERT INTO ofertas VALUES('6','2','2000','5','1999');
+
 
 INSERT INTO trabajadores VALUES('5', 'manolo@fda.tl', '234234', 'manolo', 'paco');
 INSERT INTO trabajadores VALUES('6', 'asdfmanolo@fda.tl', '32234234', 'wafdmanolo', 'fasdfpaco');
